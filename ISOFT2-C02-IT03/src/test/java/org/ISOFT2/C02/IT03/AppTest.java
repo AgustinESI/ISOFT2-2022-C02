7466a3f21b4c8c132db2e97cfacc20e3e64d7e99
Agustin Sanchez <Agustin.Sanchez9@alu.uclm.es>
2022-12-05 14:24:52 +0100

    Finalizada fase de pruebas #18

--- ISOFT2-C02-IT03/src/test/java/org/ISOFT2/C02/IT03/AppTest.java
+++ ISOFT2-C02-IT03/src/test/java/org/ISOFT2/C02/IT03/AppTest.java
@@ -1,5 +1,6 @@
 package org.ISOFT2.C02.IT03;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,10 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertEquals("Testing iteracion 3", "Testing iteracion 3");
+	}
 }
