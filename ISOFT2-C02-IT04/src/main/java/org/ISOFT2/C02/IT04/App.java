b74d897e0fd523bea5000cb97380ae133092bb61
Agustin Sanchez <Agustin.Sanchez9@alu.uclm.es>
2022-12-06 15:45:29 +0100

    Inicio iteracion 4

---
+++ ISOFT2-C02-IT04/src/main/java/org/ISOFT2/C02/IT04/App.java
@@ -0,0 +1,13 @@
+package org.ISOFT2.C02.IT04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
